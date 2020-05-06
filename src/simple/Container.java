36a7808cef4179eade21ac2a9b0220aef7670e88
Oriol Alàs Cercós <oriolalascercos@gmail.com>
2020-05-06 20:12:10 +0200

    Updated the container w/ only java 7 data structures

--- src/simple/Container.java
+++ src/simple/Container.java
@@ -2,61 +2,78 @@ package simple;
 
 import common.DependencyException;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-public class Container implements Injector{
+public class Container implements Injector {
 
 
     HashMap<String, Object> instances = new HashMap<>();
-    HashMap<String, Factory> factory = new HashMap<>();
+    HashMap<String, Factory> factories = new HashMap<>();
     HashMap<String, List<String>> dependencies = new HashMap<>();
 
     @Override
     public void registerConstant(String name, Object value) throws DependencyException {
-        if (isConstantAlreadyRegistered(name)) throw new DependencyException("Constant name is already in registered in the injector.");
+        if (isAlreadyRegistered(name))
+            throw new DependencyException("Constant name is already in registered in the injector.");
     }
 
-    private boolean isConstantAlreadyRegistered(String name) {
-        return false;
+    private boolean isAlreadyRegistered(String name) {
+        return instances.containsKey(name) || factories.containsKey(name);
     }
 
     @Override
     public void registerFactory(String name, Factory creator, String... parameters) throws DependencyException {
-        if (isFactoryAlreadyRegistered(name)) throw new DependencyException("Factory name is already in registered in the injector.");
+        if (isAlreadyRegistered(name))
+            throw new DependencyException("Factory name is already in registered in the injector.");
 
     }
 
-    private boolean isFactoryAlreadyRegistered(String name) {
-        return false;
-    }
 
     @Override
     public void registerSingleton(String name, Factory creator, String... parameters) throws DependencyException {
-        if (isFactoryAlreadyRegistered(name)) throw new DependencyException("Factory name is already in registered in the injector.");
+        if (isAlreadyRegistered(name))
+            throw new DependencyException("Factory name is already in registered in the injector.");
 
     }
 
     @Override
     public Object getObject(String name) throws DependencyException {
-        if (notRegisteredName(name))
+        if (!isAlreadyRegistered(name))
             throw new DependencyException("Given name is not registered in the injector.");
-        if (!hasAllDependenciesRegistered(name))
-            throw new DependencyException("The given name class has not all de the dependencies registered.");
         if (!objectInDependencyCycle(name))
             throw new DependencyException("The given name class is in cycle of dependencies.");
+        if (!hasAllDependenciesRegistered(name))
+            throw new DependencyException("The given name class has not all de the dependencies registered.");
         return null;
     }
 
     private boolean objectInDependencyCycle(String name) {
+        Set<String> visited = new HashSet<>();
+        Stack<String> search = new Stack<>();
+        search.push(name);
+        while (!search.isEmpty()) {
+            String currentName = search.pop();
+            if (visited.contains(currentName))
+                return true;
+            for (String dep : dependencies.get(currentName))
+                search.push(name);
+            visited.add(currentName);
+        }
         return false;
     }
 
     private boolean hasAllDependenciesRegistered(String name) {
-        return false;
+        for (String dep : dependencies.getOrDefault(name, Collections.emptyList())) {
+            if (factories.containsKey(dep)) {
+                if (!hasAllDependenciesRegistered(dep))
+                    return false;
+            } else if (!instances.containsKey(dep)) {
+                return false;
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 
-    private boolean notRegisteredName(String name) {
-        return false;
-    }
 }
